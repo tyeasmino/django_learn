2ce1b862814de42bc8c2fd099e8a9186f745dbed
tyeasmino <63666220+tyeasmino@users.noreply.github.com>
2022-05-26 03:44:09 +0600

    Correct url

--- learndShare/urls.py
+++ learndShare/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.views.static import serve
-from django.conf.urls import url 
+# from django.conf.urls import url 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
