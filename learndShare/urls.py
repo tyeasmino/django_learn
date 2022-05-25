6c93e3ae925c0b06c730b1362cc11be6cb4ad5fa
tyeasmino <63666220+tyeasmino@users.noreply.github.com>
2022-05-26 04:22:51 +0600

    Correct url

--- learndShare/urls.py
+++ learndShare/urls.py
@@ -18,8 +18,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# from django.views.static import serve
-# from django.conf.urls import url 
+from django.views.static import serve
+from django.conf.urls import url 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,7 +28,7 @@ urlpatterns = [
     path('student/', include('student.urls')),
     path('instructor/', include('instructor.urls')),
     path('', include('course.urls')), 
-    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
