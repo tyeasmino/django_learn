451482b1c2ce3e2c4abf60782881c40556fad304
tyeasmino <63666220+tyeasmino@users.noreply.github.com>
2022-05-26 04:31:20 +0600

    Correct url

--- learndShare/urls.py
+++ learndShare/urls.py
@@ -14,20 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, url 
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.views.static import serve
+# from django.views.static import serve
+# from django.conf.urls import url 
 
-urlpatterns = [
+urlpatterns = [ 
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('StudyCenter/', include('StudyCenter.urls')),
     path('student/', include('student.urls')),
-    path('instructor/', include('instructor.urls')),
+    path('instructor/', include('instructor.urls')), 
     path('', include('course.urls')), 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
