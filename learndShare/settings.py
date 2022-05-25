e0e54a98385d1eb5a9c03928bd34b5819a437ec1
tyeasmino <63666220+tyeasmino@users.noreply.github.com>
2022-05-26 04:11:35 +0600

    Correct allowed host

--- learndShare/settings.py
+++ learndShare/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-5*m%*4f6s-iw0=k@+tjg*w3_on)^5cl=8$8p752b(o+ss27&(%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
 
-ALLOWED_HOSTS = ['127.0.0.1','learndshare.herokuapp.com/']
+# ALLOWED_HOSTS = ['127.0.0.1','learndshare.herokuapp.com/']
+ALLOWED_HOSTS = ['learndshare.herokuapp.com/'] 
 
 
 # Application definition
